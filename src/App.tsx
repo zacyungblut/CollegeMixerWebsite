236dc5f378c6f9a1e689b6d063ca243588765b48
dragochar <zacyungblut@gmail.com>
2023-09-17 16:38:57 -0400

    privacy policy page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css'
 import { Route, Routes, useLocation, Link } from "react-router-dom"
 import Waitlist from './components/LandingPage/Waitlist';
 import Privacy from './components/LandingPage/Privacy';
+import Support from './components/LandingPage/Support';
+
 
 
 
@@ -16,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<><Waitlist /></>} />
         <Route path="/policy" element={<><Privacy /></>} />
+        <Route path="/support" element={<><Privacy /></>} />
       </Routes>
     </>
   )
